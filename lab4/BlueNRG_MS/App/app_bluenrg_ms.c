81775d72b7f58cb29aa976ca874fddfe840d551e
Joshua <joshuachen0213@gmail.com>
2025-10-13 14:25:42 +0800

    lab 4 add acceleratometer drivers

--- lab4/BlueNRG_MS/App/app_bluenrg_ms.c
+++ lab4/BlueNRG_MS/App/app_bluenrg_ms.c
@@ -1,39 +1,39 @@
 /**
-  ******************************************************************************
-  * @file    app_bluenrg_ms.c
-  * @author  SRA Application Team
-  * @brief   BlueNRG-M0 initialization and applicative code
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    app_bluenrg_ms.c
+ * @author  SRA Application Team
+ * @brief   BlueNRG-M0 initialization and applicative code
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "app_bluenrg_ms.h"
 
-#include "hci.h"
-#include "hci_le.h"
-#include "hci_tl.h"
-#include "link_layer.h"
-#include "sensor.h"
-#include "gatt_db.h"
-
-#include "compiler.h"
-#include "bluenrg_utils.h"
 #include "b_l475e_iot01a1.h"
 #include "bluenrg_gap.h"
 #include "bluenrg_gap_aci.h"
 #include "bluenrg_gatt_aci.h"
 #include "bluenrg_hal_aci.h"
+#include "bluenrg_utils.h"
+#include "compiler.h"
+#include "gatt_db.h"
+#include "hci.h"
+#include "hci_le.h"
+#include "hci_tl.h"
+#include "link_layer.h"
+#include "sensor.h"
 #include "sm.h"
+#include "stm32l475e_iot01_accelero.h"
 #include "stm32l4xx_hal_tim.h"
 
 /* USER CODE BEGIN Includes */
@@ -56,7 +56,7 @@ extern AxesRaw_t m_axes;
 extern AxesRaw_t q_axes;
 
 extern volatile uint8_t set_connectable;
-extern volatile int     connected;
+extern volatile int connected;
 /* at startup, suppose the X-NUCLEO-IDB04A1 is used */
 uint8_t bnrg_expansion_board = IDB04A1;
 uint8_t bdaddr[BDADDR_SIZE];
@@ -70,9 +70,7 @@ static volatile uint8_t user_button_pressed = 0;
 /* Private function prototypes -----------------------------------------------*/
 static void User_Process(void);
 static void User_Init(void);
-static void Set_Random_Environmental_Values(float *data_t, float *data_p);
-static void Set_Random_Motion_Values(uint32_t cnt);
-static void Reset_Motion_Values(void);
+void Fetch_Motion_Values(void);
 
 /* USER CODE BEGIN PFP */
 
@@ -86,145 +84,160 @@ extern volatile uint32_t ms_counter;
  * @param  None
  * @retval None
  */
-void print_csv_time(void){
-  uint32_t ms = HAL_GetTick();
-  PRINT_CSV("%02ld:%02ld:%02ld.%03ld", (long)(ms/(60*60*1000)%24), (long)(ms/(60*1000)%60), (long)((ms/1000)%60), (long)(ms%1000));
+void print_csv_time(void) {
+	uint32_t ms = HAL_GetTick();
+	PRINT_CSV("%02ld:%02ld:%02ld.%03ld",
+			  (long)(ms / (60 * 60 * 1000) % 24),
+			  (long)(ms / (60 * 1000) % 60),
+			  (long)((ms / 1000) % 60),
+			  (long)(ms % 1000));
 }
 #endif
 
-void MX_BlueNRG_MS_Init(void)
-{
-  /* USER CODE BEGIN SV */
+void MX_BlueNRG_MS_Init(void) {
+	/* USER CODE BEGIN SV */
 
-  /* USER CODE END SV */
+	/* USER CODE END SV */
 
-  /* USER CODE BEGIN BlueNRG_MS_Init_PreTreatment */
+	/* USER CODE BEGIN BlueNRG_MS_Init_PreTreatment */
 
-  /* USER CODE END BlueNRG_MS_Init_PreTreatment */
+	/* USER CODE END BlueNRG_MS_Init_PreTreatment */
 
-  /* Initialize the peripherals and the BLE Stack */
-  const char *name = "BlueNRG";
-  uint16_t service_handle, dev_name_char_handle, appearance_char_handle;
+	/* Initialize the peripherals and the BLE Stack */
+	const char *name = "BlueNRG";
+	uint16_t service_handle, dev_name_char_handle, appearance_char_handle;
 
-  uint8_t  bdaddr_len_out;
-  uint8_t  hwVersion;
-  uint16_t fwVersion;
-  int ret;
+	uint8_t bdaddr_len_out;
+	uint8_t hwVersion;
+	uint16_t fwVersion;
+	int ret;
 
-  User_Init();
+	User_Init();
 
-  /* Get the User Button initial state */
-  user_button_init_state = BSP_PB_GetState(BUTTON_KEY);
+	/* Get the User Button initial state */
+	user_button_init_state = BSP_PB_GetState(BUTTON_KEY);
 
-  hci_init(user_notify, NULL);
+	hci_init(user_notify, NULL);
 
-  /* get the BlueNRG HW and FW versions */
-  getBlueNRGVersion(&hwVersion, &fwVersion);
+	/* get the BlueNRG HW and FW versions */
+	getBlueNRGVersion(&hwVersion, &fwVersion);
 
-  /*
-   * Reset BlueNRG again otherwise we won't
-   * be able to change its MAC address.
-   * aci_hal_write_config_data() must be the first
-   * command after reset otherwise it will fail.
-   */
-  hci_reset();
-  HAL_Delay(100);
+	/*
+	 * Reset BlueNRG again otherwise we won't
+	 * be able to change its MAC address.
+	 * aci_hal_write_config_data() must be the first
+	 * command after reset otherwise it will fail.
+	 */
+	hci_reset();
+	HAL_Delay(100);
 
-  PRINTF("HWver %d\nFWver %d\n", hwVersion, fwVersion);
-  if (hwVersion > 0x30) { /* X-NUCLEO-IDB05A1 expansion board is used */
-    bnrg_expansion_board = IDB05A1;
-  }
+	PRINTF("HWver %d\nFWver %d\n", hwVersion, fwVersion);
+	if (hwVersion > 0x30) { /* X-NUCLEO-IDB05A1 expansion board is used */
+		bnrg_expansion_board = IDB05A1;
+	}
 
-  ret = aci_hal_read_config_data(CONFIG_DATA_RANDOM_ADDRESS, BDADDR_SIZE, &bdaddr_len_out, bdaddr);
+	ret = aci_hal_read_config_data(
+		CONFIG_DATA_RANDOM_ADDRESS, BDADDR_SIZE, &bdaddr_len_out, bdaddr);
 
-  if (ret) {
-    PRINTF("Read Static Random address failed.\n");
-  }
+	if (ret) {
+		PRINTF("Read Static Random address failed.\n");
+	}
 
-  if ((bdaddr[5] & 0xC0) != 0xC0) {
-    PRINTF("Static Random address not well formed.\n");
-    while(1);
-  }
+	if ((bdaddr[5] & 0xC0) != 0xC0) {
+		PRINTF("Static Random address not well formed.\n");
+		while (1)
+			;
+	}
 
-  /* GATT Init */
-  ret = aci_gatt_init();
-  if(ret){
-    PRINTF("GATT_Init failed.\n");
-  }
+	/* GATT Init */
+	ret = aci_gatt_init();
+	if (ret) {
+		PRINTF("GATT_Init failed.\n");
+	}
 
-  /* GAP Init */
-  if (bnrg_expansion_board == IDB05A1) {
-    ret = aci_gap_init_IDB05A1(GAP_PERIPHERAL_ROLE_IDB05A1, 0, 0x07, &service_handle, &dev_name_char_handle, &appearance_char_handle);
-  }
-  else {
-    ret = aci_gap_init_IDB04A1(GAP_PERIPHERAL_ROLE_IDB04A1, &service_handle, &dev_name_char_handle, &appearance_char_handle);
-  }
-  if (ret != BLE_STATUS_SUCCESS) {
-    PRINTF("GAP_Init failed.\n");
-  }
+	/* GAP Init */
+	if (bnrg_expansion_board == IDB05A1) {
+		ret = aci_gap_init_IDB05A1(GAP_PERIPHERAL_ROLE_IDB05A1,
+								   0,
+								   0x07,
+								   &service_handle,
+								   &dev_name_char_handle,
+								   &appearance_char_handle);
+	} else {
+		ret = aci_gap_init_IDB04A1(GAP_PERIPHERAL_ROLE_IDB04A1,
+								   &service_handle,
+								   &dev_name_char_handle,
+								   &appearance_char_handle);
+	}
+	if (ret != BLE_STATUS_SUCCESS) {
+		PRINTF("GAP_Init failed.\n");
+	}
 
-  /* Update device name */
-  ret = aci_gatt_update_char_value(service_handle, dev_name_char_handle, 0,
-                                   strlen(name), (uint8_t *)name);
-  if (ret) {
-    PRINTF("aci_gatt_update_char_value failed.\n");
-    while(1);
-  }
+	/* Update device name */
+	ret = aci_gatt_update_char_value(
+		service_handle, dev_name_char_handle, 0, strlen(name), (uint8_t *)name);
+	if (ret) {
+		PRINTF("aci_gatt_update_char_value failed.\n");
+		while (1)
+			;
+	}
 
-  ret = aci_gap_set_auth_requirement(MITM_PROTECTION_REQUIRED,
-                                     OOB_AUTH_DATA_ABSENT,
-                                     NULL,
-                                     7,
-                                     16,
-                                     USE_FIXED_PIN_FOR_PAIRING,
-                                     123456,
-                                     BONDING);
-  if (ret) {
-    PRINTF("aci_gap_set_authentication_requirement failed.\n");
-    while(1);
-  }
+	ret = aci_gap_set_auth_requirement(MITM_PROTECTION_REQUIRED,
+									   OOB_AUTH_DATA_ABSENT,
+									   NULL,
+									   7,
+									   16,
+									   USE_FIXED_PIN_FOR_PAIRING,
+									   123456,
+									   BONDING);
+	if (ret) {
+		PRINTF("aci_gap_set_authentication_requirement failed.\n");
+		while (1)
+			;
+	}
 
-  PRINTF("BLE Stack Initialized\n");
+	PRINTF("BLE Stack Initialized\n");
 
-  ret = Add_HWServW2ST_Service();
-  if(ret == BLE_STATUS_SUCCESS) {
-    PRINTF("BlueMS HW service added successfully.\n");
-  } else {
-    PRINTF("Error while adding BlueMS HW service: 0x%02x\r\n", ret);
-    while(1);
-  }
+	ret = Add_HWServW2ST_Service();
+	if (ret == BLE_STATUS_SUCCESS) {
+		PRINTF("BlueMS HW service added successfully.\n");
+	} else {
+		PRINTF("Error while adding BlueMS HW service: 0x%02x\r\n", ret);
+		while (1)
+			;
+	}
 
-  ret = Add_SWServW2ST_Service();
-  if(ret == BLE_STATUS_SUCCESS) {
-     PRINTF("BlueMS SW service added successfully.\n");
-  } else {
-     PRINTF("Error while adding BlueMS HW service: 0x%02x\r\n", ret);
-     while(1);
-  }
+	ret = Add_SWServW2ST_Service();
+	if (ret == BLE_STATUS_SUCCESS) {
+		PRINTF("BlueMS SW service added successfully.\n");
+	} else {
+		PRINTF("Error while adding BlueMS HW service: 0x%02x\r\n", ret);
+		while (1)
+			;
+	}
 
-  /* Set output power level */
-  ret = aci_hal_set_tx_power_level(1,4);
+	/* Set output power level */
+	ret = aci_hal_set_tx_power_level(1, 4);
 
-  /* USER CODE BEGIN BlueNRG_MS_Init_PostTreatment */
+	/* USER CODE BEGIN BlueNRG_MS_Init_PostTreatment */
 
-  /* USER CODE END BlueNRG_MS_Init_PostTreatment */
+	/* USER CODE END BlueNRG_MS_Init_PostTreatment */
 }
 
 /*
  * BlueNRG-MS background task
  */
-void MX_BlueNRG_MS_Process(void)
-{
-  /* USER CODE BEGIN BlueNRG_MS_Process_PreTreatment */
+void MX_BlueNRG_MS_Process(void) {
+	/* USER CODE BEGIN BlueNRG_MS_Process_PreTreatment */
 
-  /* USER CODE END BlueNRG_MS_Process_PreTreatment */
+	/* USER CODE END BlueNRG_MS_Process_PreTreatment */
 
-  User_Process();
-  hci_user_evt_proc();
+	User_Process();
+	hci_user_evt_proc();
 
-  /* USER CODE BEGIN BlueNRG_MS_Process_PostTreatment */
+	/* USER CODE BEGIN BlueNRG_MS_Process_PostTreatment */
 
-  /* USER CODE END BlueNRG_MS_Process_PostTreatment */
+	/* USER CODE END BlueNRG_MS_Process_PostTreatment */
 }
 
 /**
@@ -233,12 +246,11 @@ void MX_BlueNRG_MS_Process(void)
  * @param  None
  * @retval None
  */
-static void User_Init(void)
-{
-  BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_EXTI);
-  BSP_LED_Init(LED2);
+static void User_Init(void) {
+	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_EXTI);
+	BSP_LED_Init(LED2);
 
-  BSP_COM_Init(COM1);
+	BSP_COM_Init(COM1);
 }
 
 /**
@@ -248,144 +260,60 @@ static void User_Init(void)
  * @param  None
  * @retval None
  */
-static void User_Process(void)
-{
-  float data_t;
-  float data_p;
-  static uint32_t counter = 0;
-
-  if (set_connectable)
-  {
-    Set_DeviceConnectable();
-    set_connectable = FALSE;
-  }
+static void User_Process(void) {
+	if (set_connectable) {
+		Set_DeviceConnectable();
+		set_connectable = FALSE;
+	}
 
 #if USE_BUTTON
-  /* Check if the user has pushed the button */
-  if (user_button_pressed)
-  {
-    /* Debouncing */
-    HAL_Delay(50);
+	/* Check if the user has pushed the button */
+	if (user_button_pressed) {
+		/* Debouncing */
+		HAL_Delay(50);
 
-    /* Wait until the User Button is released */
-    while (BSP_PB_GetState(BUTTON_KEY) == !user_button_init_state);
+		/* Wait until the User Button is released */
+		while (BSP_PB_GetState(BUTTON_KEY) == !user_button_init_state)
+			;
 
-    /* Debouncing */
-    HAL_Delay(50);
+		/* Debouncing */
+		HAL_Delay(50);
 #endif
-    BSP_LED_Toggle(LED2);
+		BSP_LED_Toggle(LED2);
 
-    if (connected)
-    {
-      /* Set a random seed */
-      srand(HAL_GetTick());
+		if (connected) {
+			/* Set a random seed */
+			srand(HAL_GetTick());
 
-      /* Update emulated Environmental data */
-      Set_Random_Environmental_Values(&data_t, &data_p);
-      BlueMS_Environmental_Update((int32_t)(data_p *100), (int16_t)(data_t * 10));
+			/* Update emulated Acceleration, Gyroscope and Sensor Fusion data */
+			Fetch_Motion_Values();
+			Acc_Update(&x_axes);
 
-      /* Update emulated Acceleration, Gyroscope and Sensor Fusion data */
-      Set_Random_Motion_Values(counter);
-      Acc_Update(&x_axes, &g_axes, &m_axes);
-      Quat_Update(&q_axes);
-
-      counter ++;
-      if (counter == 40) {
-        counter = 0;
-        Reset_Motion_Values();
-      }
 #if !USE_BUTTON
-      HAL_Delay(1000); /* wait 1 sec before sending new data */
+			HAL_Delay(1000); /* wait 1 sec before sending new data */
 #endif
-    }
+		}
 #if USE_BUTTON
-    /* Reset the User Button flag */
-    user_button_pressed = 0;
-  }
+		/* Reset the User Button flag */
+		user_button_pressed = 0;
+	}
 #endif
 }
 
-/**
- * @brief  Set random values for all environmental sensor data
- * @param  float pointer to temperature data
- * @param  float pointer to pressure data
- * @retval None
- */
-static void Set_Random_Environmental_Values(float *data_t, float *data_p)
-{
-  *data_t = 27.0 + ((uint64_t)rand()*5)/RAND_MAX;     /* T sensor emulation */
-  *data_p = 1000.0 + ((uint64_t)rand()*80)/RAND_MAX; /* P sensor emulation */
+void Fetch_Motion_Values() {
+	int16_t pDataXYZ[3];
+	BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+	x_axes.AXIS_X = pDataXYZ[0];
+	x_axes.AXIS_Y = pDataXYZ[2];
+	x_axes.AXIS_Z = pDataXYZ[1];
 }
 
 /**
- * @brief  Set random values for all motion sensor data
- * @param  uint32_t counter for changing the rotation direction
- * @retval None
+ * @brief  BSP Push Button callback
+ * @param  Button Specifies the pin connected EXTI line
+ * @retval None.
  */
-static void Set_Random_Motion_Values(uint32_t cnt)
-{
-  /* Update Acceleration, Gyroscope and Sensor Fusion data */
-  if (cnt < 20) {
-    x_axes.AXIS_X +=  (10  + ((uint64_t)rand()*3*cnt)/RAND_MAX);
-    x_axes.AXIS_Y += -(10  + ((uint64_t)rand()*5*cnt)/RAND_MAX);
-    x_axes.AXIS_Z +=  (10  + ((uint64_t)rand()*7*cnt)/RAND_MAX);
-    g_axes.AXIS_X +=  (100 + ((uint64_t)rand()*2*cnt)/RAND_MAX);
-    g_axes.AXIS_Y += -(100 + ((uint64_t)rand()*4*cnt)/RAND_MAX);
-    g_axes.AXIS_Z +=  (100 + ((uint64_t)rand()*6*cnt)/RAND_MAX);
-    m_axes.AXIS_X +=  (3  + ((uint64_t)rand()*3*cnt)/RAND_MAX);
-    m_axes.AXIS_Y += -(3  + ((uint64_t)rand()*4*cnt)/RAND_MAX);
-    m_axes.AXIS_Z +=  (3  + ((uint64_t)rand()*5*cnt)/RAND_MAX);
-
-    q_axes.AXIS_X -= (100  + ((uint64_t)rand()*3*cnt)/RAND_MAX);
-    q_axes.AXIS_Y += (100  + ((uint64_t)rand()*5*cnt)/RAND_MAX);
-    q_axes.AXIS_Z -= (100  + ((uint64_t)rand()*7*cnt)/RAND_MAX);
-  }
-  else {
-    x_axes.AXIS_X += -(10  + ((uint64_t)rand()*3*cnt)/RAND_MAX);
-    x_axes.AXIS_Y +=  (10  + ((uint64_t)rand()*5*cnt)/RAND_MAX);
-    x_axes.AXIS_Z += -(10  + ((uint64_t)rand()*7*cnt)/RAND_MAX);
-    g_axes.AXIS_X += -(100 + ((uint64_t)rand()*2*cnt)/RAND_MAX);
-    g_axes.AXIS_Y +=  (100 + ((uint64_t)rand()*4*cnt)/RAND_MAX);
-    g_axes.AXIS_Z += -(100 + ((uint64_t)rand()*6*cnt)/RAND_MAX);
-    m_axes.AXIS_X += -(3  + ((uint64_t)rand()*7*cnt)/RAND_MAX);
-    m_axes.AXIS_Y +=  (3  + ((uint64_t)rand()*9*cnt)/RAND_MAX);
-    m_axes.AXIS_Z += -(3  + ((uint64_t)rand()*3*cnt)/RAND_MAX);
-
-    q_axes.AXIS_X += (200 + ((uint64_t)rand()*7*cnt)/RAND_MAX);
-    q_axes.AXIS_Y -= (150 + ((uint64_t)rand()*3*cnt)/RAND_MAX);
-    q_axes.AXIS_Z += (10  + ((uint64_t)rand()*5*cnt)/RAND_MAX);
-  }
-
-}
-
-/**
- * @brief  Reset values for all motion sensor data
- * @param  None
- * @retval None
- */
-static void Reset_Motion_Values(void)
-{
-  x_axes.AXIS_X = (x_axes.AXIS_X)%2000 == 0 ? -x_axes.AXIS_X : 10;
-  x_axes.AXIS_Y = (x_axes.AXIS_Y)%2000 == 0 ? -x_axes.AXIS_Y : -10;
-  x_axes.AXIS_Z = (x_axes.AXIS_Z)%2000 == 0 ? -x_axes.AXIS_Z : 10;
-  g_axes.AXIS_X = (g_axes.AXIS_X)%2000 == 0 ? -g_axes.AXIS_X : 100;
-  g_axes.AXIS_Y = (g_axes.AXIS_Y)%2000 == 0 ? -g_axes.AXIS_Y : -100;
-  g_axes.AXIS_Z = (g_axes.AXIS_Z)%2000 == 0 ? -g_axes.AXIS_Z : 100;
-  m_axes.AXIS_X = (g_axes.AXIS_X)%2000 == 0 ? -m_axes.AXIS_X : 3;
-  m_axes.AXIS_Y = (g_axes.AXIS_Y)%2000 == 0 ? -m_axes.AXIS_Y : -3;
-  m_axes.AXIS_Z = (g_axes.AXIS_Z)%2000 == 0 ? -m_axes.AXIS_Z : 3;
-  q_axes.AXIS_X = -q_axes.AXIS_X;
-  q_axes.AXIS_Y = -q_axes.AXIS_Y;
-  q_axes.AXIS_Z = -q_axes.AXIS_Z;
-}
-
-/**
-  * @brief  BSP Push Button callback
-  * @param  Button Specifies the pin connected EXTI line
-  * @retval None.
-  */
-void BSP_PB_Callback(Button_TypeDef Button)
-{
-  /* Set the User Button flag */
-  user_button_pressed = 1;
+void BSP_PB_Callback(Button_TypeDef Button) {
+	/* Set the User Button flag */
+	user_button_pressed = 1;
 }
--- lab4/BlueNRG_MS/App/gatt_db.h
+++ lab4/BlueNRG_MS/App/gatt_db.h
@@ -1,26 +1,27 @@
 /**
-  ******************************************************************************
-  * @file    App/gatt_db.h
-  * @author  SRA Application Team
-  * @brief   Header file for App/gatt_db.c
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    App/gatt_db.h
+ * @author  SRA Application Team
+ * @brief   Header file for App/gatt_db.c
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 #ifndef GATT_DB_H
 #define GATT_DB_H
 
 #include <stdint.h>
 #include <stdlib.h>
+
 #include "bluenrg_def.h"
 
 #define X_OFFSET 200
@@ -42,41 +43,38 @@
  * @brief Structure containing acceleration value of each axis.
  */
 typedef struct {
-  int32_t AXIS_X;
-  int32_t AXIS_Y;
-  int32_t AXIS_Z;
+	int32_t AXIS_X;
+	int32_t AXIS_Y;
+	int32_t AXIS_Z;
 } AxesRaw_t;
 
-enum {
-  ACCELERATION_SERVICE_INDEX = 0,
-  ENVIRONMENTAL_SERVICE_INDEX = 1
-};
+enum { ACCELERATION_SERVICE_INDEX = 0, ENVIRONMENTAL_SERVICE_INDEX = 1 };
 
 /** Documentation for C union Service_UUID_t */
 typedef union Service_UUID_t_s {
-  /** 16-bit UUID
-  */
-  uint16_t Service_UUID_16;
-  /** 128-bit UUID
-  */
-  uint8_t Service_UUID_128[16];
+	/** 16-bit UUID
+	 */
+	uint16_t Service_UUID_16;
+	/** 128-bit UUID
+	 */
+	uint8_t Service_UUID_128[16];
 } Service_UUID_t;
 
 /** Documentation for C union Char_UUID_t */
 typedef union Char_UUID_t_s {
-  /** 16-bit UUID
-  */
-  uint16_t Char_UUID_16;
-  /** 128-bit UUID
-  */
-  uint8_t Char_UUID_128[16];
+	/** 16-bit UUID
+	 */
+	uint16_t Char_UUID_16;
+	/** 128-bit UUID
+	 */
+	uint8_t Char_UUID_128[16];
 } Char_UUID_t;
 
 tBleStatus Add_HWServW2ST_Service(void);
 tBleStatus Add_SWServW2ST_Service(void);
 void Read_Request_CB(uint16_t handle);
 tBleStatus BlueMS_Environmental_Update(int32_t press, int16_t temp);
-tBleStatus Acc_Update(AxesRaw_t *x_axes, AxesRaw_t *g_axes, AxesRaw_t *m_axes);
+tBleStatus Acc_Update(AxesRaw_t *x_axes);
 tBleStatus Quat_Update(AxesRaw_t *q_axes);
 
 extern uint8_t Services_Max_Attribute_Records[];
