488814809e7a627d23ca6b7cb46f7dfefb807daf
Joshua <joshuachen0213@gmail.com>
2025-10-15 23:31:27 +0800

    lab4 scanner add threading

--- lab4/Central/scanner.py
+++ lab4/Central/scanner.py
@@ -1,7 +1,18 @@
-from bluepy.btle import Scanner, DefaultDelegate, Peripheral
 import time
-from prompt_toolkit import prompt
+import threading
+
+from prompt_toolkit import Application
+from prompt_toolkit.layout import Layout, HSplit
+from prompt_toolkit.widgets import TextArea
+from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.patch_stdout import patch_stdout
+
+from bluepy.btle import DefaultDelegate, Peripheral, Scanner
+
+CCCD_UUID = "00002902-0000-1000-8000-00805f9b34fb"
+TARGET_UUID = "11110000-1111-0000-1111-000011110000"
+TARGET_SERVICE_UUID = "00000000-0001-11e1-9ab4-0002a5d5c51b"
+TARGET_CHAR_UUID = "00e00000000111e1ac360002a5d5c51b"
 
 
 class ScanDelegate(DefaultDelegate):
@@ -16,20 +27,8 @@ class ScanDelegate(DefaultDelegate):
             return
 
 
-class MyDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleNotification(self, cHandle, data):
-        x = int.from_bytes(data[2:4], byteorder="little", signed=True)
-        y = int.from_bytes(data[4:6], byteorder="little", signed=True)
-        z = int.from_bytes(data[6:8], byteorder="little", signed=True)
-        print(f"Get notification {cHandle}: {x}, {y}, {z}")
-        return
-
-
 def connect():
-    print("-" * 10, "start of scan", "-" * 10)
+    print("-" * 10 + " start of scan " + "-" * 10)
     scanner = Scanner().withDelegate(ScanDelegate())
     while True:
         try:
@@ -38,7 +37,7 @@ def connect():
         except:
             pass
 
-    print("\n\r", "-" * 10, "end of scan", "-" * 10)
+    print("\n" + "-" * 11 + " end of scan " + "-" * 11)
 
     for dev in devices:
         complete_local_name = dev.getValueText(9)
@@ -55,35 +54,43 @@ def connect():
 
         print("-" * 30)
         peripheral = Peripheral(dev)
-        peripheral.setDelegate(MyDelegate())
+
         return peripheral
+
     raise AssertionError("Device not found")
+
+
+def scan(peripheral):
+    services = peripheral.getServices()
+    services = [s for s in services if s.uuid == TARGET_SERVICE_UUID]
+    service = services[0]
+
+    characteristics = service.getCharacteristics()
+    characteristics = [c for c in characteristics if c.uuid == TARGET_CHAR_UUID]
+    c = characteristics[0]
+
+    prop = c.propertiesToString()
+    handle = c.getHandle()
+    print(f" {c}: {prop}, {handle}")
+    print("-" * 33)
+
+    return handle
+
+
+def get_handle(peripheral, char_handle, UUID):
+    descriptors = peripheral.getDescriptors(char_handle, 0xFFFF)
+    for d in descriptors:
+        if str(d.uuid).lower() == UUID.lower():
+            return d.handle
 
 
 def main():
     peripheral = connect()
-    CCCD_UUID = "00002902-0000-1000-8000-00805f9b34fb"
-    target_c_uuid = "11110000-1111-0000-1111-000011110000"
-    services = peripheral.getServices()
-    services = [s for s in services if s.uuid == "00000000-0001-11e1-9ab4-0002a5d5c51b"]
-    service = services[0]
-    characteristics = service.getCharacteristics()
-    cccd_handle = None
+    char_handle = scan(peripheral)
+    cccd_handle = get_handle(peripheral, char_handle, CCCD_UUID)
+    target_handle = get_handle(peripheral, char_handle, TARGET_UUID)
 
-    characteristics = [
-        c for c in characteristics if c.uuid == "00e00000000111e1ac360002a5d5c51b"
-    ]
-    c = characteristics[0]
-    prop = c.propertiesToString()
-    handle = c.getHandle()
-    print(f" {c}: {prop}, {handle}")
-    descriptors = peripheral.getDescriptors(handle, 0xFFFF)
-    for d in descriptors:
-        # print("   ", str(d.uuid).lower())
-        if str(d.uuid).lower() == CCCD_UUID.lower() and cccd_handle == None:
-            cccd_handle = d.handle
-        if str(d.uuid).lower() == target_c_uuid.lower():
-            target_c_handle = d.handle
+    print("-" * 4 + " turning on notification " + "-" * 4)
     if cccd_handle is not None:
         print(f"        Found CCCD handle: {cccd_handle}")
         peripheral.writeCharacteristic(cccd_handle, b"\x01\x00", False)
@@ -91,25 +98,67 @@ def main():
         time.sleep(1)
         ret = peripheral.readCharacteristic(cccd_handle)
         print(f"current value: {ret}")
+    print("-" * 33)
 
-    print("-" * 30)
+    def ble_notification_simulator():
+        while True:
+            if peripheral.waitForNotifications(1.0):
+                time.sleep(0.1)
 
-    while True:
-        if peripheral.waitForNotifications(1.0):
-            time.sleep(1)
-            continue
-        """
-        with patch_stdout():
-            freq = prompt("set the freq here > ")
+    log_area = TextArea(style="class:output-field", scrollbar=True, focusable=False)
+    input_field = TextArea(
+        height=1, prompt="freq > ", style="class:input-field", multiline=False
+    )
 
-            if freq.isdigit():
-                freq = int(freq)
-                freq_bytes = freq.to_bytes(4, byteorder='little')
-                if target_c_handle:
-                    peripheral.writeCharacteristic(target_c_handle, freq_bytes, withResponse=False)
-            else:
-                print("Wrong input format")
-        """
+    root_container = HSplit([log_area, input_field])
+    layout = Layout(root_container, focused_element=input_field)
+
+    kb = KeyBindings()
+
+    @kb.add("enter")
+    def _handle_standard_input(event):
+        user_input = input_field.text.strip()
+        input_field.text = ""
+        if user_input == "" or not user_input.isdigit():
+            log_area.buffer.insert_text(f"[USER] Invalid Input\n")
+            event.app.invalidate()
+            return
+
+        freq = int(user_input)
+        freq_bytes = freq.to_bytes(4, byteorder="little")
+        if freq > 20:
+            log_area.buffer.insert_text(f"[USER] Frequency Too Large, cap to {20}\n")
+            freq = 20
+        else:
+            log_area.buffer.insert_text(f"[USER] Set Frequency to {freq}\n")
+        peripheral.writeCharacteristic(target_handle, freq_bytes, withResponse=False)
+        event.app.invalidate()
+
+    @kb.add("c-c")
+    def _keyboard_interrupt(event):
+        event.app.exit(exception=KeyboardInterrupt, style="class:aborting")
+
+    class MyDelegate(DefaultDelegate):
+        def __init__(self):
+            DefaultDelegate.__init__(self)
+
+        def handleNotification(self, cHandle, data):
+            x = int.from_bytes(data[2:4], byteorder="little", signed=True)
+            y = int.from_bytes(data[4:6], byteorder="little", signed=True)
+            z = int.from_bytes(data[6:8], byteorder="little", signed=True)
+            if app.is_running:
+                log_area.buffer.insert_text(
+                    f"[BLE] Get notification {cHandle}: {x}, {y}, {z}\n"
+                )
+                app.invalidate()
+            return
+
+    app = Application(layout=layout, key_bindings=kb, full_screen=True)
+    peripheral.setDelegate(MyDelegate())
+    threading.Thread(target=ble_notification_simulator, daemon=True).start()
+
+    with patch_stdout():
+        app.run()
 
 
 if __name__ == "__main__":
