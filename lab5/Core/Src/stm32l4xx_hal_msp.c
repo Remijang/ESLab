e2712e48b209edb93cc6d32f034e6d9b86d46fe2
remijang <wuet0829@gmail.com>
2025-10-16 18:25:25 +0800

    update hw5

--- lab5/Core/Src/stm32l4xx_hal_msp.c
+++ lab5/Core/Src/stm32l4xx_hal_msp.c
@@ -428,14 +428,14 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**TIM2 GPIO Configuration
-    PA5     ------> TIM2_CH1
+    PA15 (JTDI)     ------> TIM2_CH1
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Pin = LED1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
 
     /* USER CODE BEGIN TIM2_MspPostInit 1 */
 
@@ -461,9 +461,9 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
 
     /**TIM2 GPIO Configuration
     PA3     ------> TIM2_CH4
-    PA5     ------> TIM2_CH1
+    PA15 (JTDI)     ------> TIM2_CH1
     */
-    HAL_GPIO_DeInit(GPIOA, ARD_D4_Pin|GPIO_PIN_5);
+    HAL_GPIO_DeInit(GPIOA, ARD_D4_Pin|LED1_Pin);
 
     /* USER CODE BEGIN TIM2_MspDeInit 1 */
 
